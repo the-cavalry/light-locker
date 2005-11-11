07de22afdc37355a13f296c8e67754cd067ecdb5
William Jon McCann <mccann@jhu.edu>
2005-11-11 22:03:05 +0000

    Make window OverrideRedirect.
    2005-11-11  William Jon McCann  <mccann@jhu.edu>
    
    	* src/gs-window-x11.c (gs_window_new):
    	Make window OverrideRedirect.
    
    	Patch from Ray Strode <rstrode@redhat.com>
    	Fixes #321194.

--- src/gs-window-x11.c
+++ src/gs-window-x11.c
@@ -1286,6 +1286,7 @@ gs_window_new (GdkScreen *screen,
         GObject     *result;
 
         result = g_object_new (GS_TYPE_WINDOW,
+                               "type", GTK_WINDOW_POPUP,
                                "screen", screen,
                                "monitor", monitor,
                                "lock-enabled", lock_enabled,
