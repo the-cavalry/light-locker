75ab977a424ed341139f6c7e64561d37e56ea7c7
William Jon McCann <mccann@jhu.edu>
2006-11-06 17:49:50 +0000

    Only notice mouse motion of 10% of screen width instead of 50px.
    2006-11-06  William Jon McCann  <mccann@jhu.edu>
    
    	* src/gs-window-x11.c: (gs_window_real_motion_notify_event):
    	Only notice mouse motion of 10% of screen width instead of 50px.
    
    	Patch from Thomas Andersen <phomes@gmail.com>
    	Fixes #359589

--- src/gs-window-x11.c
+++ src/gs-window-x11.c
@@ -1743,11 +1743,16 @@ static gboolean
 gs_window_real_motion_notify_event (GtkWidget      *widget,
                                     GdkEventMotion *event)
 {
-        GSWindow *window;
-        gdouble   distance;
-        gdouble   min_distance = 50;
+        GSWindow  *window;
+        gdouble    distance;
+        gdouble    min_distance;
+        gdouble    min_percentage = 0.1;
+        GdkScreen *screen;
 
         window = GS_WINDOW (widget);
+
+        screen = gs_window_get_screen (window);
+        min_distance = gdk_screen_get_width (screen) * min_percentage;
 
         /* if the last position was not set then don't detect motion */
         if (window->priv->last_x < 0 || window->priv->last_y < 0) {
