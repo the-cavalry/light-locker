a0182767d8ef5498206d6eb096a0e11ad4ece2dd
William Jon McCann <mccann@jhu.edu>
2006-12-11 20:54:12 +0000

    Added script to help run gdb from outside the GNOME session.
    2006-12-11  William Jon McCann  <mccann@jhu.edu>
    
    	* src/Makefile.am:
    	* src/debug-screensaver.sh:
    	Added script to help run gdb from outside
    	the GNOME session.

---
+++ src/debug-screensaver.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This is probably linux only at the moment
+
+if [ -z "${DBUS_SESSION_BUS_ADDRESS}" ]; then
+    pid=`pgrep gnome-session`
+    if [ "x$pid" != "x" ]; then
+        env_address=`(cat /proc/$pid/environ; echo) | tr "\000" "\n" | grep '^DBUS_SESSION_BUS_ADDRESS='`
+        env_display=`(cat /proc/$pid/environ; echo) | tr "\000" "\n" | grep '^DISPLAY='`
+        if [ "x$env_address" != "x" ]; then
+            echo "Setting $env_address"
+            echo "Setting $env_display"
+            eval "export $env_address"
+            eval "export $env_display"
+        fi
+    fi
+fi
+
+if [ -z "${DBUS_SESSION_BUS_ADDRESS}" ]; then
+    echo "Could not determine DBUS_SESSION_BUS_ADDRESS"
+    exit 1
+fi
+
+# kill the existing daemon
+gnome-screensaver-command --exit
+
+# run the daemon in the debugger
+gdb --args gnome-screensaver --no-daemon --debug --sync
